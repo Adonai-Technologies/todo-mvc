bf0db329b5594e1bb6ff54b076800509fb7f2958
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2024-01-02 17:33:34 +0100

    added some style

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const express = require('express')
-const app = express()
-const connectDB = require('./config/database')
+const express = require('express')// Importing the Express library/module
+const app = express()// Creating an instance of the Express application
+const connectDB = require('./config/database') // Importing database connection function from a file
+
+// Importing route handlers for home and todos
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'})// Loading environment variables from a configuration file
 
-connectDB()
+// Connecting to the database
+ connectDB()
 
+// Setting the view engine for rendering dynamic content using EJS
 app.set('view engine', 'ejs')
+
+// Serving static files from the 'public' directory
 app.use(express.static('public'))
+
+// Parsing incoming requests with URL-encoded payloads
 app.use(express.urlencoded({ extended: true }))
+
+// Parsing incoming requests with JSON payloads
 app.use(express.json())
 
+// Attaching the homeRoutes handler for requests at '/'
 app.use('/', homeRoutes)
+
+// Attaching the todoRoutes handler for requests at '/todos'
 app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
+
+// Starting the server to listen for incoming requests on the specified port
+app.listen(process.env.PORT, () => {
     console.log('Server is running, you better catch it!')
-})    
+})
